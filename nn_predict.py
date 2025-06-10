b1d4ac60b43fea5c1a88649701ad82bf6c89e210
113598057 <kxxx88888@gmail.com>
2025-06-10 14:12:09 +0800

    Add files via upload

--- nn_predict.py
+++ nn_predict.py
@@ -8,8 +8,8 @@ def relu(x):
 
 def softmax(x):
     # TODO: Implement the SoftMax function
-    exps = np.exp(x - np.max(x, axis=1, keepdims=True))  
-    return exps / np.sum(exps, axis=1, keepdims=True)
+    e = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    return e / np.sum(e, axis=-1, keepdims=True)
 
 # === Flatten ===
 def flatten(x):
